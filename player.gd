c05b1c91e15ac42162a18fd7b7fbf061ea0af51a
KerimShen <skerim395@gmail.com>
2026-03-29 16:08:59 +0300

    karakter görseli oyuna eklendi

--- player.gd
+++ player.gd
@@ -2,26 +2,21 @@ extends CharacterBody2D
 
 const SPEED = 200.0
 
-func _ready():
-	print("SCRIPT CALISIYOR")
-	var rect = ColorRect.new()
-	rect.size = Vector2(50, 50)
-	rect.position = Vector2(-25, -25)
-	rect.color = Color.BLUE
-	add_child(rect)
-	
 var can_attack = true
+
+func _ready():
+	pass
 
 func _process(delta):
 	if Input.is_action_just_pressed("attack") and can_attack:
 		attack()
-		
+
 func attack():
-	can_attack = false 
+	can_attack = false
 	print("SALDIRI!")
 	await get_tree().create_timer(0.5).timeout
-	can_attack = true		
-		
+	can_attack = true
+
 func _physics_process(delta):
 	var direction = Vector2.ZERO
 	
@@ -33,9 +28,6 @@ func _physics_process(delta):
 		direction.y += 1
 	if Input.is_action_pressed("move_up"):
 		direction.y -= 1
-	
-	velocity = direction.normalized() * SPEED
-	move_and_slide()
 	
 	velocity = direction.normalized() * SPEED
 	move_and_slide()
